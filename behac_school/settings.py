bd508753b62fbef16670b85f6d6055dbf5d73b19
0715173877 <cmihanjo@gmail.com>
2026-05-04 12:32:34 +0300

    settings

--- behac_school/settings.py
+++ behac_school/settings.py
@@ -133,9 +133,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
+
+# Directory where static files will be collected
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional directories for static files
+#STATICFILES_DIRS = [
+#    BASE_DIR.parent / 'pejuni.com',
+#]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/login/'
